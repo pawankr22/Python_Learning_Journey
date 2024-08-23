466a3ae4e970725985b83ebd234d233313ae02aa
Pawan Kumar <cnct.pawan@gmail.com>
2024-08-23 18:23:51 +0530

    functions completed

---
+++ Functions/Storing_your_functions_in_modules/making_pizzas.py
@@ -0,0 +1,9 @@
+# Importing the module "pizza.py"
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# We can also import a specific function from a module. 
+# Ex: from module_name import function_name // This can be syntax to import any function
